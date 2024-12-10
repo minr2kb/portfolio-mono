01fc9baf619d06ff8dc97f4e60004a65ab9d30c4
Kyungbae Min <kbmin1129@gmail.com>
2024-12-10 15:50:29 +0900

    v4 컨텐츠 추가

--- apps/v4/src/consts/sectionData.ts
+++ apps/v4/src/consts/sectionData.ts
@@ -57,8 +57,8 @@ export const experiencesData: ExperienceItemType[] = [
     subtitle: '전 제로클래스랩, 2022.07 - 2023.07',
     logoSrc: '/images/logo/tmrfounders.png',
     descs: [
-      '원스톱 창업 플랫폼 “내일의창업” 웹 개발 및 런칭',
-      'NFC 기반 스마트 오더 솔루션 “태그히어” 서비스 제작',
+      '원스톱 창업 플랫폼 "내일의창업" 웹 개발 및 런칭',
+      'NFC 기반 스마트 오더 솔루션 "태그히어" 서비스 제작',
       '아이템 태거 등 주요 컴포넌트 패키지 제작',
       '스타트업 서비스의 초기 기획부터 배포까지의 사이클 경험',
     ],
@@ -163,11 +163,11 @@ export const skillsData: SkillItemType[] = [
     rating: 3,
     image: '/images/stackIcon/Webpack.webp',
   },
-  {
-    name: 'EC2',
-    rating: 2,
-    image: '/images/stackIcon/EC2.webp',
-  },
+  //   {
+  //     name: 'EC2',
+  //     rating: 2,
+  //     image: '/images/stackIcon/EC2.webp',
+  //   },
   {
     name: 'NodeJS',
     rating: 4,
@@ -197,6 +197,11 @@ export const skillsData: SkillItemType[] = [
     name: 'Supabase',
     rating: 3,
     image: '/images/stackIcon/Supabase.webp',
+  },
+  {
+    name: 'Storybook',
+    rating: 3,
+    image: '/images/stackIcon/Storybook.webp',
   },
 ];
 
@@ -259,6 +264,70 @@ export const projectsData: ProjectItemType[] = [
     image: '/images/projects/careerly.webp',
     link: 'https://careerly.co.kr',
     skills: ['ReactJS', 'NextJS', 'ReactNative', 'TypeScript', 'TailwindCSS', 'Recoil', 'Tanstack-Query', 'Redux'],
+    cateTag: 'PUBLY',
+  },
+  {
+    id: 'careerly-design-system',
+    title: '커리어리 디자인 시스템',
+    description: `커리어리 서비스의 디자인/토큰 시스템`,
+    details: [
+      {
+        content: '디자인 시스템 자동화 및 통합',
+        children: [
+          {
+            content: '디자인 시스템의 일관성과 효율성을 높이기 위해 디자인 토큰과 아이콘 관리 자동화 제안 및 구현',
+          },
+          {
+            content:
+              'Figma 플러그인(Tokens Studio, Icona)과 Style Dictionary를 활용하여 디자인 토큰 및 아이콘을 웹과 앱에 맞게 변환 및 배포',
+          },
+          {
+            content:
+              'Bitbucket Pipelines를 이용하여 PR 머지 시 자동으로 토큰과 아이콘이 배포되는 CI/CD 파이프라인 구축',
+          },
+        ],
+      },
+      {
+        content: '프로덕트 성능 및 효율 개선',
+        children: [
+          {
+            content:
+              'ThemeProvider 및 유틸리티 함수를 개발하여 스타일링을 템플릿화하고 유지보수 및 개발 시간을 30% 단축',
+          },
+          {
+            content: '디자인 시스템 변경 사항이 즉각적으로 모든 플랫폼에 반영되도록 프로세스 최적화',
+          },
+          {
+            content: '디자인 토큰 서브모듈화를 통해 코드베이스와 디자인 자산 간의 연계성을 강화',
+          },
+        ],
+      },
+      {
+        content: '효율적인 협업 환경 구축',
+        children: [
+          {
+            content: '디자이너와 개발자 간의 원활한 협업을 위해 디자인 자산의 중앙 집중식 관리 환경 제공',
+          },
+          {
+            content: '설계와 구현 간의 간극을 줄이기 위한 기술 문서 작성',
+          },
+        ],
+      },
+    ],
+    image: '/images/projects/careerly-design-system.webp',
+    link: 'https://github.com/publy-korea/design-tokens',
+    skills: [
+      'ReactJS',
+      'ReactNative',
+      'TypeScript',
+      'TailwindCSS',
+      'NodeJS',
+      'CVA',
+      'Tokens Studio',
+      'Bitbucket',
+      'Storybook',
+      'Figma',
+    ],
     cateTag: 'PUBLY',
   },
   {
@@ -370,16 +439,38 @@ export const projectsData: ProjectItemType[] = [
     description: `포트폴리오 버전관리를 위한 MFE 모노레포`,
     details: [
       {
-        content: 'PNPM + Turborepo를 통해 모노레포 환경 구축 및 템플릿+스크립트를 통해 프로젝트 스캐폴딩 구현',
+        content: '확장성 있는 모노레포 구조',
+        children: [
+          {
+            content:
+              'PNPM + Turborepo를 활용한 모노레포 환경 구성과 템플릿 기반 스캐폴딩 cli로 새로운 버전의 포트폴리오 앱을 독립 환경에서 빠르게 생성 가능',
+          },
+          {
+            content: '자체 제작 type-build-util 패키지를 통한 리모트 앱 타입 정의 자동 추출',
+          },
+        ],
       },
       {
-        content: 'module-federation을 통해 각 버전의 앱을 독립적으로 배포',
+        content: '마이크로 프론트엔드 아키텍처',
+        children: [
+          {
+            content: 'Vite Module Federation을 활용한 런타임 통합으로 각 버전의 포트폴리오를 독립적으로 개발/배포 가능',
+          },
+          {
+            content: 'Cache-control을 통한 리모트 앱 변경사항 즉시 반영',
+          },
+        ],
       },
       {
-        content: 'type-build-utils 패키지를 제작하여 host앱에서 remote 모듈의 타입 정의를 사용할 수 있도록 구성',
-      },
-      {
-        content: 'Cache-control을 통해 리모트 변경 사항이 바로 반영되도록 구성',
+        content: '최적화된 개발 경험',
+        children: [
+          {
+            content: '독립적인 개발 서버 구동으로 빠른 HMR 지원 및 공통 템플릿 스캐폴딩을 통한 일관된 프로젝트 구조',
+          },
+          {
+            content: 'Firebase Hosting의 CDN 기반 배포와 getAssetsUrl() 유틸을 통해 remote앱의 에셋을 안정적으로 로드',
+          },
+        ],
       },
     ],
     image: '/images/projects/portfolio-mono.webp',
@@ -387,11 +478,71 @@ export const projectsData: ProjectItemType[] = [
     skills: ['ReactJS', 'TypeScript', 'Vite', 'MUI', 'ChakraUI', 'PNPM', 'Turborepo', 'Firebase'],
     cateTag: '사이드',
   },
-
+  {
+    id: 'christmas-tree-decor',
+    title: '크리스마스 트리 꾸미기',
+    description: `QR을 활용해 실시간 스크린의 크리스마스 트리를 꾸미는 서비스`,
+    details: [
+      {
+        content: '실시간 인터랙션 구현',
+        children: [
+          {
+            content: 'Supabase Realtime을 활용한 실시간 데이터 동기화',
+          },
+          {
+            content: 'animationQueue 구현을 통한 자연스러운 조건부 애니메이션 구현',
+          },
+          {
+            content: 'WebRTC 기반 QR 코드 스캔과 원격 조작을 통한 트리 제어기능 구현',
+          },
+        ],
+      },
+      {
+        content: '보안 및 사용자 경험 최적화',
+        children: [
+          {
+            content: 'Row Level Security (RLS)와 Aho-Corasick 알고리즘 기반 욕설 필터링으로 안전한 익명 접속 환경 구축',
+          },
+          {
+            content: 'CSS transform 활용 성능 최적화 및 토스트 메시지를 통한 직관적인 사용자 피드백 제공',
+          },
+        ],
+      },
+      {
+        content: '개발자 도구 및 편의성 기능',
+        children: [
+          {
+            content: 'QR 코드 생성/다운로드, 공유 링크 복사 등 편의 기능 구현',
+          },
+          {
+            content: '전체화면, UI 요소 제어 등 다양한 단축키 지원으로 개발자 경험 향상',
+          },
+        ],
+      },
+      {
+        content: '효율적인 기술 선택',
+        children: [
+          {
+            content: 'Tanstack-query, Nextjs 등을 고려했으나, 실시간 인터렉션 지원을 위해 React + jotai로 진행',
+          },
+          {
+            content: 'api는 네임스페이스 분리를 위해 class로 감싸 활용',
+          },
+          {
+            content: 'chakraUI v3를 활용하여 필요한 컴포넌트를 받아 사용',
+          },
+        ],
+      },
+    ],
+    image: '/images/projects/christmas-tree-decor.webp',
+    skills: ['ReactJS', 'TypeScript', 'ChakraUI', 'Supabase'],
+    link: 'https://github.com/minr2kb/christmas-tree-decor',
+    cateTag: '사이드',
+  },
   {
     id: 'tradeforce',
     title: '트레이드포스',
-    description: `수출입 전문가 매칭 플랫폼, ‘트레이드포스’ 외주 개발`,
+    description: `수출입 전문가 매칭 플랫폼, '트레이드포스' 외주 개발`,
     details: [
       {
         content: '서비스 의뢰 페이지 부터, 의뢰를 관리하는 어드민 페이지 내부 퍼널을 구성하기 위한 고민들을 경험',
@@ -415,16 +566,46 @@ export const projectsData: ProjectItemType[] = [
     description: `구직 웹사이트 채용공고들을 한번에 모아보기 위한 데스크톱 스크래핑 앱`,
     details: [
       {
-        content: 'Koa로 구성된 크롤링 서버를 CloudType 서비스를 활용하여 배포',
+        content: '효율적인 크롤링 아키텍처 설계',
+        children: [
+          {
+            content: 'Koa 기반 크롤링 서버를 CloudType에 배포하여 안정적인 서비스 제공',
+          },
+          {
+            content: '카테고리 수에 따른 동기/비동기 처리 전환으로 서버 부하 최적화 (5개 기준)',
+          },
+          {
+            content: 'AbortController를 활용한 크롤링 중단/재시작 기능 구현',
+          },
+        ],
       },
       {
-        content: 'request 갯수에 상한선을 두어, 동기/비동기 요청을 별개로 주도록 만들어 서버 부하를 방지',
+        content: '데스크톱 앱 개발 및 배포 자동화',
+        children: [
+          {
+            content: 'electron-updater와 Github Release를 활용한 자동 업데이트 파이프라인 구축',
+          },
+          {
+            content: 'IPC 통신으로 메인-렌더러 프로세스 간 로컬 API 구현',
+          },
+          {
+            content: 'Vite를 활용한 빌드 최적화 및 코드 스플리팅',
+          },
+        ],
       },
       {
-        content: 'github에 release를 배포하여 자동 업데이트가 가능하도록 구현',
-      },
-      {
-        content: '간단한 ipc통신으로 로컬 api를 제작하였으며, AbortController를 활용하여 알맞 요청 취소를 구현',
+        content: '사용자 중심 기능 구현',
+        children: [
+          {
+            content: '6개 주요 채용 플랫폼 통합 검색 지원 (잡코리아, 원티드 등)',
+          },
+          {
+            content: '플랫폼/날짜별 필터링 및 CSV 형식 결과 다운로드 기능',
+          },
+          {
+            content: '크롤링 진행상황 실시간 모니터링 UI 제공',
+          },
+        ],
       },
     ],
     image: '/images/projects/recruit-crawler.webp',
@@ -446,47 +627,86 @@ export const projectsData: ProjectItemType[] = [
     ],
     image: '/images/projects/worship-team-live.webp',
     skills: ['ReactJS', 'TypeScript', 'MUI', 'Firebase'],
-    link: 'https://worship-team-live.web.app',
+    link: 'https://github.com/minr2kb/worship-team-live',
     cateTag: '사이드',
   },
-  {
-    id: 'christmas-tree-decor',
-    title: '크리스마스 트리 꾸미기',
-    description: `QR을 활용해 실시간 스크린의 크리스마스 트리를 꾸미는 서비스`,
-    details: [
-      {
-        content: 'Supabase를 통해 실시간 장식에 대한 데이터 저장 및 실시간 업데이트 기능 구현',
-      },
-      {
-        content: '로그인 없는 익명 접속을 위해 RLS를 통한 접근 제어 및 클라이언트 단 비속어 필터링 구현',
-      },
-      {
-        content: 'QR 다운로드, 디버깅 단축키 등 편의성 기능 구현',
-      },
-    ],
-    image: '/images/projects/christmas-tree-decor.webp',
-    skills: ['ReactJS', 'TypeScript', 'ChakraUI', 'Supabase'],
-    link: 'https://christmas-tree-decor.web.app',
-    cateTag: '사이드',
-  },
+
   {
     id: 'publy-dinner-worldcup',
     title: '퍼블리 회식 월드컵',
     description: `회사 근처 회식장소 선정을 위한 프로젝트`,
     details: [
       {
-        content: 'Firebase를 통한 서버리스 환경 개발',
+        content: '토너먼트 투표 시스템 구현',
+        children: [
+          {
+            content: '1:1 대결 방식의 토너먼트 구조로 최종 우승 식당을 선정',
+          },
+          {
+            content: '회식 참여자/비참여자 구분 투표 및 포인트 시스템을 통한 순위 산정',
+          },
+          {
+            content: '라운드별 애니메이션 효과로 게임적 요소 강화',
+          },
+        ],
       },
       {
-        content: '장소별/유저별 등 결과 필터링 기능 구현',
+        content: 'Firebase 기반 실시간 데이터 관리',
+        children: [
+          {
+            content: '실시간 투표 결과 반영 및 장소별/유저별 결과 필터링 기능 구현',
+          },
+          {
+            content: '투표 이력 관리와 결과 테이블 제공으로 투명한 집계 시스템 구축',
+          },
+        ],
       },
       {
-        content: '재치있는 UI를 위해 캐릭터 등의 UI 요소 추가',
+        content: '인터랙티브 UI/UX 구현',
+        children: [
+          {
+            content: '음식 이모지가 내리는 애니메이션 효과로 생동감 있는 화면 연출',
+          },
+          {
+            content: '귀여운 개미 캐릭터와 상황별 멘트로 사용자 친근감 향상',
+          },
+          {
+            content: '데스크톱 최적화 UI 및 모바일 접속 시 안내 메시지 제공',
+          },
+        ],
       },
     ],
     image: '/images/projects/publy-dinner-worldcup.webp',
     skills: ['ReactJS', 'TypeScript', 'MUI', 'Firebase'],
-    link: 'https://publy-dinner-worldcup.web.app',
+    link: 'https://github.com/minr2kb/publy-dinner-worldcup',
+    cateTag: '사이드',
+  },
+  {
+    id: 'inf-scroll',
+    title: '가상 무한 스크롤',
+    description: `가상 무한 스크롤 구현 및 비교 연습`,
+    details: [
+      {
+        content: '가상 무한 스크롤 직접 구현',
+        children: [
+          {
+            content: 'IntersectionObserver를 사용하여 Inifinite Scroll 구현',
+          },
+          {
+            content: 'Vitualizer을 구현하여 성능 최적화 및 렌더링 효율성 향상',
+          },
+          {
+            content: '높이 측정 및 캐싱을 통해 동적 높이의 아이템 관리',
+          },
+          {
+            content: '이진 검색을 통한 인덱스 계산으로 스크롤 성능 최적화',
+          },
+        ],
+      },
+    ],
+    image: '/images/projects/inf-scroll.webp',
+    link: 'https://github.com/minr2kb/infinite-scroll-practice',
+    skills: ['React.js', 'TypeScript', 'ChakraUI', 'Jotai', 'React-Virtuoso'],
     cateTag: '사이드',
   },
   {
@@ -532,7 +752,6 @@ export const projectsData: ProjectItemType[] = [
       {
         content: '각 앱들은 소형 사이드 프로젝트들로 구성',
       },
-
       {
         content:
           'Webpack의 require.context를 활용하여 NextJS처럼 개발환경의 컴포넌트 디렉토리 구조가 그대로 브라우저에 렌더링 되도록 환경을 구축',
